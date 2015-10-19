73ca376d4b46b26855c24eb394c7dc4613347c9a
Ben Ashman <bash@bash-mbp.local>
2015-10-18 18:59:09 -0700

    handle shortcuts from background state

--- ProTip/AppDelegate.swift
+++ ProTip/AppDelegate.swift
@@ -72,7 +72,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        print("application did become active!")
+        
+        guard let shortcut = shortcutItem else { return }
+        
+        print("shortcut property has been set")
+        
+        handleQuickAction(shortcut)
+        
+        self.shortcutItem = nil
     }
 
     func applicationWillTerminate(application: UIApplication) {
