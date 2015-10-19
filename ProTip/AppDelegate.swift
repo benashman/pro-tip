4cd8c477776a73734c0ddfcb6a558343ea987722
Ben Ashman <bash@bash-mbp.local>
2015-10-18 18:55:24 -0700

    add quick action handler

--- ProTip/AppDelegate.swift
+++ ProTip/AppDelegate.swift
@@ -12,12 +12,50 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var shortcutItem: UIApplicationShortcutItem?
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+        var performShortcutDelegate = true
+        
+        if let shortcutItem = launchOptions?[UIApplicationLaunchOptionsShortcutItemKey] as? UIApplicationShortcutItem {
+            print("application launched via. shortcut")
+            self.shortcutItem = shortcutItem
+            
+            performShortcutDelegate = false
+        }
+        
+        return performShortcutDelegate
     }
+    
+    // MARK: Quick Actions
+    
+    func application(application: UIApplication, performActionForShortcutItem shortcutItem: UIApplicationShortcutItem, completionHandler: (Bool) -> Void) {
+        completionHandler(handleQuickAction(shortcutItem))
+    }
+    
+    func handleQuickAction(shortcutItem: UIApplicationShortcutItem) -> Bool {
+        print("handling shortcut!")
+        
+        var succeeded = false
+        
+        switch shortcutItem.type {
+        case "split-2":
+            print("splitting bill 2 ways")
+            succeeded = true
+        case "split-3":
+            print("splitting bill 3 ways")
+            succeeded = true
+        case "split-4":
+            print("splitting bill 4 ways")
+            succeeded = true
+        default:
+            succeeded = false
+        }
+        
+        return succeeded
+    }
+    
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
